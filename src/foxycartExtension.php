097f15edaa4e99d5e9d826b45cdc09ba4c02d063
WeIntendit AB <santino.petrovic@intendit.se>
2021-04-07 09:05:30 +0200

    Added stockupdate for SSL and stock-check url

--- src/foxycartExtension.php
+++ src/foxycartExtension.php
@@ -28,6 +28,8 @@ class foxycartExtension extends SimpleExtension
     {
         // All requests to /koala
         $collection->match('/foxycartstockupdate', [$this, 'callbackKoalaCatching']);
+        $collection->match('/foxycartstockupdateSSL', [$this, 'foxycartStockUpdateSSL']);
+        $collection->match('/foxycartstockCheckSSL', [$this, 'foxycartstockCheckSSL']);
     }
 
 
@@ -187,7 +189,241 @@ class foxycartExtension extends SimpleExtension
             return;
         }
     }
+    public function foxycartStockUpdateSSL(Application $app, Request $request)
+    {    
 
+        $config = $this->getConfig();
+
+        define('FOXY_WEBHOOK_ENCRYPTION_KEY', $config['encryption_key']);
+        $data = file_get_contents('php://input');
+        $parsedData = json_decode($data, true);
+        $event = $_SERVER['HTTP_FOXY_WEBHOOK_EVENT'];
+         
+        // Verify the webhook payload
+        $signature = hash_hmac('sha256', $data, FOXY_WEBHOOK_ENCRYPTION_KEY);
+        if (!hash_equals($signature, $_SERVER['HTTP_FOXY_WEBHOOK_SIGNATURE'])) {
+            echo "Signature verification failed - data corrupted";
+            http_response_code(500);
+            return;
+        }         
+            if (is_array($parsedData)) {
+                $test = 0;
+                if (isset($parsedData["_embedded"]["fx:items"])) {
+                    foreach ($parsedData["_embedded"]["fx:items"] as $items) {
+                        $code = $items["code"];
+                        $quantity = $items["quantity"];
+
+                        // Check if the product contains attributes
+                        if (isset($items["_embedded"]["fx:item_options"])) {
+                            try {
+
+                                // Find the product
+                                $repo = $app['storage']->getRepository($config['contenttype_foxy']);
+                                $qb = $repo->createQueryBuilder();
+                                $qb->where('title="'.$items['code'].'"');
+                                $products = $repo->findOneWith($qb);        
+                                $prodId = $products["id"];
+                                $repoAttr = $app['storage']->getRepository('attributkategorier');
+                                $qb = $repoAttr->createQueryBuilder();
+                                $attrCat = $qb->execute()->fetchAll();
+                                $attrArr = [];
+
+                                // Loop the webhook's attributes and find the right quantity column to calculate value of
+                                foreach ($items["_embedded"]["fx:item_options"] as $attrs) {
+                                    $catName = $attrs["name"];
+                                    $catValue = $attrs["value"];
+
+                                    $query = $app['db']->createQueryBuilder()
+                                        ->select('*')
+                                        ->from('bolt_'.$config["contenttype_category"])
+                                        ->where('title LIKE "'.$catName.'"');
+                                    $resultscats = $query->execute()->fetchAll();
+
+                                    $query = $app['db']->createQueryBuilder()
+                                        ->select('*')
+                                        ->from('bolt_'.$config["contenttype_values"])
+                                        ->where('title LIKE "'.$catValue.'"');
+                                    $results = $query->execute()->fetchAll();
+
+                                    foreach ($results as $result) {
+                                        $valueId = $result["id"];
+                                        foreach ($resultscats as $valuecats) {
+                                            $resultIds = json_decode($valuecats["selectattribute"]);
+                                            if(in_array($valueId, $resultIds)) {
+                                                array_push($attrArr, $valueId);
+                                            }
+                                        }
+                                    }
+
+                                }
+                                sort($attrArr);
+                                $attrArr = json_encode($attrArr);
+                                $query = $app['db']->createQueryBuilder()
+                                    ->select('*')
+                                    ->from('bolt_field_value')
+                                    ->where("value_json_array LIKE '".$attrArr."'")
+                                    ->andWhere("content_id LIKE '".$prodId."'");
+                                $checkresults = $query->execute()->fetchAll();            
+                                foreach ($checkresults as $groupitem) {
+                                    $query = $app['db']->createQueryBuilder()
+                                        ->select('*')
+                                        ->from('bolt_field_value')
+                                        ->where("fieldname LIKE 'quantity'")
+                                        ->andWhere("content_id LIKE '".$prodId."'")
+                                        ->andWhere("grouping LIKE '".$groupitem["grouping"]."'");
+                                    $quantityresults = $query->execute()->fetchAll();
+                                    foreach ($quantityresults as $quantityresult) {
+                                        $currentQuantity = $quantityresult["value_integer"] - $quantity;
+                                        $query = $app['db']->createQueryBuilder()
+                                            ->update('bolt_field_value')
+                                            ->set('value_integer', $currentQuantity)
+                                            ->where("fieldname LIKE 'quantity'")
+                                            ->andWhere("content_id LIKE '".$prodId."'")
+                                            ->andWhere("grouping LIKE '".$groupitem["grouping"]."'");
+                                        $query->execute();                
+                                    }
+                                }
+                            } catch (Exception $e) {
+                                // If something wrong with handling the data with this extension, return 500.
+                                http_response_code(500);
+                                return;
+                            }
+                        } else { 
+
+                            // If no attributes on product, change the main quantity value in db
+                            $repo = $app['storage']->getRepository($config['contenttype_foxy']);
+                            $qb = $repo->createQueryBuilder();
+                            $qb->where('title="'.$code.'"');
+                            $products = $repo->findOneWith($qb);
+
+                            $currentQuantity = $products["quantity"] - $quantity;
+                            $query = $app['db']->createQueryBuilder()
+                                ->update('bolt_'.$config['contenttype_foxy'])
+                                ->set('quantity', $currentQuantity)
+                                ->where('id = '.$products["id"].'');
+                            $query->execute();                            
+                        }
+                    }
+                }
+                return true;
+            } else {
+                // Data is corrupted, send response 500 to foxycart.
+                echo("Encrypted data corrupted");
+                http_response_code(500);
+                return;
+            }
+    }
+
+    public function foxycartstockCheckSSL(Application $app, Request $request)
+    {    
+
+        $config = $this->getConfig();
+        define('FOXY_WEBHOOK_ENCRYPTION_KEY', $config['encryption_key']);
+        $data = file_get_contents('php://input');
+        $parsedData = json_decode($data, true);
+        $response = array(
+            'ok' => true,
+            'details' => 'Sorry, one of the products in your cart is either out of stock or less quantity than in your cart. Please remove the items from your cart and try again.'
+        );      
+            if (is_array($parsedData)) {              
+                if (isset($parsedData["_embedded"]["fx:items"])) {
+                    foreach ($parsedData["_embedded"]["fx:items"] as $items) {
+                        $code = $items["code"];
+                        $quantity = $items["quantity"];
+                        // Check if the product contains attributes
+                        if (isset($items["_embedded"]["fx:item_options"])) {                            
+                            try {
+                                $myfile = fopen("/var/bmss/source/3.2foxycart/extensions/vendor/intendit/foxycart/test.txt", "w");
+                                fwrite($myfile, 'tes1');
+                                fclose($myfile);                                
+                                // Find the product
+                                $repo = $app['storage']->getRepository($config['contenttype_foxy']);
+                                $qb = $repo->createQueryBuilder();
+                                $qb->where('title="'.$items['code'].'"');
+                                $products = $repo->findOneWith($qb);        
+                                $prodId = $products["id"];
+                                $repoAttr = $app['storage']->getRepository('attributkategorier');
+                                $qb = $repoAttr->createQueryBuilder();
+                                $attrCat = $qb->execute()->fetchAll();
+                                $attrArr = [];
+
+                                // Loop the webhook's attributes and find the right quantity column to calculate value of
+                                foreach ($items["_embedded"]["fx:item_options"] as $attrs) {
+                                    $catName = $attrs["name"];
+                                    $catValue = $attrs["value"];
+
+                                    $query = $app['db']->createQueryBuilder()
+                                        ->select('*')
+                                        ->from('bolt_'.$config["contenttype_category"])
+                                        ->where('title LIKE "'.$catName.'"');
+                                    $resultscats = $query->execute()->fetchAll();
+
+                                    $query = $app['db']->createQueryBuilder()
+                                        ->select('*')
+                                        ->from('bolt_'.$config["contenttype_values"])
+                                        ->where('title LIKE "'.$catValue.'"');
+                                    $results = $query->execute()->fetchAll();
+
+                                    foreach ($results as $result) {
+                                        $valueId = $result["id"];
+                                        foreach ($resultscats as $valuecats) {
+                                            $resultIds = json_decode($valuecats["selectattribute"]);
+                                            if(in_array($valueId, $resultIds)) {
+                                                array_push($attrArr, $valueId);
+                                            }
+                                        }
+                                    }
+
+                                }
+                                sort($attrArr);
+                                $attrArr = json_encode($attrArr);
+                                $query = $app['db']->createQueryBuilder()
+                                    ->select('*')
+                                    ->from('bolt_field_value')
+                                    ->where("value_json_array LIKE '".$attrArr."'")
+                                    ->andWhere("content_id LIKE '".$prodId."'");
+                                $checkresults = $query->execute()->fetchAll();          
+                                foreach ($checkresults as $groupitem) {
+                                    $query = $app['db']->createQueryBuilder()
+                                        ->select('*')
+                                        ->from('bolt_field_value')
+                                        ->where("fieldname LIKE 'quantity'")
+                                        ->andWhere("content_id LIKE '".$prodId."'")
+                                        ->andWhere("grouping LIKE '".$groupitem["grouping"]."'");
+                                    $quantityresults = $query->execute()->fetchAll();
+                                    foreach ($quantityresults as $quantityresult) {
+                                        $currentQuantity = $quantityresult["value_integer"] - $quantity;
+                                        if ($currentQuantity < 0) {
+                                            $response['ok'] = false;
+                                        }
+                                    }
+                                }
+                            } catch (Exception $e) {
+                                $response['ok'] = false;
+                            }
+                        } else {                             
+                            // If no attributes on product, change the main quantity value in db
+                            $repo = $app['storage']->getRepository($config['contenttype_foxy']);
+                            $qb = $repo->createQueryBuilder();
+                            $qb->where('title="'.$code.'"');
+                            $products = $repo->findOneWith($qb);
+                            $currentQuantity = $products["quantity"] - $quantity;                         
+                            if ($currentQuantity < 0) {
+                                $response['ok'] = false;
+                            }
+                        }
+                    }
+                
+                }
+            } else {            
+                $respons['ok'] = false;
+            }
+            header('Content-Type: application/json');
+            $response = new Response(json_encode($response));
+            $response->headers->set('Content-Type', 'application/json');
+            
+            return $response;            
+    }
     protected function registerTwigFunctions()
     {
         return [
